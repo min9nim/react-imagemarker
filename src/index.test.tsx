843f33eb7df18d371acab7a3e8ad0a164ebc3e1e
keating <mgsong@madup.com>
2021-07-23 18:35:22 +0900

    update testcase

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,7 @@
-import { ExampleComponent } from '.'
+import ImageMarker from '.'
 
 describe('ExampleComponent', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+    expect(ImageMarker).toBeTruthy()
   })
 })
