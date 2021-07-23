bd9d310e541c08acc5505f4a5593e9306a3e4206
keating <mgsong@madup.com>
2021-07-23 17:47:19 +0900

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-imagemarker'
+import 'react-imagemarker/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
