ccdf841da6dcbe781ee5de01e4423f0eae18baf0
keating <mgsong@madup.com>
2021-07-23 19:28:13 +0900

    update image

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,17 +6,17 @@ import 'react-imagemarker/dist/index.css'
 const markers = [
   {
     x: 20,
-    y: 20,
+    y: 40,
     id: 1,
-  },
-  {
-    x: 50,
-    y: 20,
-    id: 2,
   },
   {
     x: 40,
     y: 40,
+    id: 2,
+  },
+  {
+    x: 70,
+    y: 55,
     id: 3,
     render: () => <div style={{fontSize: 30}}>🙂</div>
   },
@@ -26,7 +26,7 @@ const App = () => {
   return (
     <div style={{ width: 500, height: 500 }}>
       <ImageMarker
-        src="https://www.mountainprofessor.com/images/matterhorn-3-b.jpg"
+        src="https://lever.me/statics/image/introduce/Group16.svg"
         markers={markers}
         onImageClick={console.log}
       />
