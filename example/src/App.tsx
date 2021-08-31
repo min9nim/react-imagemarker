2332807e01af7b82afcba8e64cef1ebde4631450
keating <mgsong@madup.com>
2021-08-31 15:47:19 +0900

    more example images

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -18,7 +18,7 @@ const markers = [
     x: 70,
     y: 55,
     id: 3,
-    render: () => <div style={{fontSize: 30}}>🙂</div>
+    render: () => <div style={{fontSize: 30}}>🙂</div> // eslint-disable-line
   },
 ]
 
@@ -26,7 +26,9 @@ const App = () => {
   return (
     <div style={{ width: 500, height: 500 }}>
       <ImageMarker
-        src="https://lever.me/statics/image/introduce/Group16.svg"
+        // src="https://lever.me/statics/image/introduce/Group16.svg"
+        src="https://lever-admin-develop.s3.ap-northeast-2.amazonaws.com/shops/16/assets/416/design/300/kura-goddin-5JEbKNp9uNk-unsplash.jpg" // 세로이미지
+        // src="https://lever-admin-develop.s3.ap-northeast-2.amazonaws.com/shops/16/assets/421/design/296/%EB%8F%84%EB%9D%BC%EC%97%90%EB%AA%BD.gif" // 가로이미지
         markers={markers}
         onImageClick={console.log}
       />
